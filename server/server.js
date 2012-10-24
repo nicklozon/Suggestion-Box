eb4552913e7868e0e7480bf7bd350d8cf72c5fb3
Nick Lozon <nicklozon@gmail.com>
2012-10-23 21:03:49 -0400

    Base client web resources and server resource

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ server.use(restify.acceptParser(server.acceptable));
 server.use(restify.queryParser());
 server.use(restify.bodyParser());
 
-server.get('/echo/:name', function (req, rest,next) {
+server.get('/suggestion_box/echo/:name', function (req, res,next) {
     res.send(req.params);
     return next();
 });
